3721c005bc2d9915c2245122cc5ad463a488bb38
Bass El Hachem <contact@kompo.io>
2024-12-10 09:42:01 -0300

    Update OidcClient.php to debug dev environment

--- src/OidcClient.php
+++ src/OidcClient.php
@@ -230,8 +230,11 @@ class OidcClient implements OidcClientInterface
     $jsonData = mb_convert_encoding($jsonData, 'UTF-8');
 
     // Read the data
-    //$data = json_decode($jsonData, true);
-    $data = OidcJwtHelper::parseToken($jsonData)->claims()->all();
+    if(str_contains($_SERVER['HTTP_HOST'], '.local')){
+        $data = OidcJwtHelper::parseToken($jsonData)->claims()->all();
+    } else {
+        $data = json_decode($jsonData, true);
+    }
 
     // Check data due
     if (!is_array($data)) {
